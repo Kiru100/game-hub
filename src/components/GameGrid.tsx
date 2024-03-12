f70f36c3ccb27f54351f75729847d2d2ccb18cf7
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-03-12 19:39:18 +0800

    refactor implementing infinite scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,37 +1,38 @@
-import { Box, Button, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { GameQuery } from "../App";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
-import React from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-	const { data, error, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } = useGames(gameQuery);
+	const { data, error, isLoading, fetchNextPage, hasNextPage } = useGames(gameQuery);
 	const skeletons = [1, 2, 3, 4, 5, 6];
 
   	if (error) return <Text>{error.message}</Text>;
 
 	return (
 		<InfiniteScroll
-			dataLength={(data?.pages.length) ? data?.pages.length : 0}
+			dataLength={data?.pages.reduce((total, page) => total + page.results.length, 0) || 0}
 			next={()=>fetchNextPage()}
-			hasMore={hasNextPage}
-			loader={<h4>Loading...</h4>}
+			hasMore={!!hasNextPage}
+			loader={<Spinner />}
 			endMessage={
 				<p style={{ textAlign: 'center' }}>
-				  <b>Yay! You have seen it all</b>
+					<b>Yay! You have seen it all</b>
 				</p>
 			}
 		>
 			<SimpleGrid
 				columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
 				spacing={6}
+				p="10px"
 			>
 			{isLoading &&
 				skeletons.map((skeleton) => (
